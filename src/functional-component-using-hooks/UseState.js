38479ef31065bcbcb303c76040f46265cc42982f
talhanousher <talhanousher@gmail.com>
2020-11-28 00:26:06 +0500

    functional compoenent

--- src/functional-component-using-hooks/UseState.js
+++ src/functional-component-using-hooks/UseState.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const UseStateComponent = ()=> (<div>
-    Hello functional component
-</div>)
+const UseStateComponent = () => {
+  const [firstName,setFirstName] = useState('');
+  const [lastName,setLastName] = useState('');
+
+  const handleFirstNameChange = (e)=>{
+    setFirstName(e.target.value);
+  }
+  const handleLastNameChange = (e)=>{
+    setLastName(e.target.value);
+  }
+
+  return <div className="functional-component">
+    <div>
+      <label>
+        First Name :
+        <input value={firstName} onChange={handleFirstNameChange} placeholder="First Name" />
+      </label>
+    </div>
+    <br />
+    <div>
+      <label>
+        Last Name :
+        <input value={lastName} onChange={handleLastNameChange} placeholder="Last Name" />
+      </label>
+    </div>
+    <br />
+    <div>Display First Name : {firstName}</div>
+    <div>Display First Name :  {lastName}</div>
+  </div>
+}
 
 export default UseStateComponent;
