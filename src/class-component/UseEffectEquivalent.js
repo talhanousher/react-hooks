d639217e3f744462d9646fd910477513b4655642
talhanousher <talhanousher@gmail.com>
2020-11-28 19:37:35 +0500

    unmounting and use effect

--- src/class-component/UseEffectEquivalent.js
+++ src/class-component/UseEffectEquivalent.js
@@ -4,15 +4,24 @@ class UseEffectEquivalentComponent extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			count: 0
+			count: 0,
+			width: window.innerWidth
 		}
 		this.handleCountChange = this.handleCountChange.bind(this);
 	}
 	componentDidMount() {
 		document.title = `${this.state.count} times clicked`;
+		window.addEventListener('resize', () => {
+			this.setState({
+				width: window.innerWidth
+			})
+		})
 	}
 	componentDidUpdate() {
 		document.title = `${this.state.count} times clicked`;
+	}
+	componentWillUnmount() {
+		window.removeEventListener('resize');
 	}
 	handleCountChange() {
 		this.setState({
@@ -22,6 +31,9 @@ class UseEffectEquivalentComponent extends Component {
 	render() {
 		return <div className="class-component">
 			<button onClick={this.handleCountChange} >Click Me!</button>
+			<div>
+				WIDTH: {this.state.width}
+			</div>
 		</div>
 	}
 }
--- src/functional-component-using-hooks/UseEffect.js
+++ src/functional-component-using-hooks/UseEffect.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectComponent = () => {
   const [count, setCount] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
 
   const handleCountChange = () => {
     setCount(count + 1);
@@ -9,9 +10,20 @@ const UseEffectComponent = () => {
   useEffect(() => {
     document.title = `${count} times`
   });
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setWidth(window.innerWidth);
+    });
+    return () => {
+      window.removeEventListener('resize');
+    }
+  }, []);
 
   return <div className="functional-component">
     <button onClick={handleCountChange}>Click Me!</button>
+    <div>
+      WIDTH: {width}
+    </div>
   </div>
 }
 
